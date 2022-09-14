e825b2e8dd80abfc84c803d579f8dda752b7b547
Muhammad Ali <reddragon33795@gmail.com>
2022-09-14 02:40:02 -0700

    Simple Html Layout

---
+++ src/index.js
@@ -0,0 +1,32 @@
+/* Object
+let name = {
+    man : 'lala',
+    nude : 'person'
+};
+
+console.log(name.nude);
+console.log(name.nude,name.man);
+*/
+
+/*Array
+var firstArray = [10];
+for (let index = 0; index < firstArray.length; index++) {
+        firstArray[index] = firstArray[index + 1];
+};
+
+for (let index = 0; index < firstArray.length; index++) {
+    console.log(firstArray[index]);  
+};
+*/
+/*Function Add
+function add(value1,value2) {
+    return value1 + value2;
+}
+console.log(add(2,3));
+*/
+
+/*Arrays*/
+var myArray = [22,33];
+
+
+console.log(result);
