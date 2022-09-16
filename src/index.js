689d5a042f8ee510eeb286e4744b531063bb44ae
Muhammad Ali <reddragon33795@gmail.com>
2022-09-16 08:54:31 -0700

    Used (display) for Out put

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 //Grabing Buttons
-let displayI;
+let displayI = document.getElementById('display');
 let button = document.querySelectorAll('button');
 console.log(button);
+
+
 
 //To Go through all Buttons in node list i used For loop(For Each can be used however)
 
 for (let i = 0; i < button.length; i++) {
     //console.log(button[i].innerHTML);
     button[i].addEventListener("click", (show) => {
-        displayI = button[i].innerHTML;
-        document.getElementById('display').value += displayI;
+        displayI.value += button[i].innerHTML;
     })
 }
