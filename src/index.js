968c60acefa17f55a2c98c2516ba2afe62a17439
Muhammad Ali <reddragon33795@gmail.com>
2022-09-16 08:42:11 -0700

    Input Display Done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-//Branch hello Origin Branch
+//Grabing Buttons
+let displayI;
+let button = document.querySelectorAll('button');
+console.log(button);
+
+//To Go through all Buttons in node list i used For loop(For Each can be used however)
+
+for (let i = 0; i < button.length; i++) {
+    //console.log(button[i].innerHTML);
+    button[i].addEventListener("click", (show) => {
+        displayI = button[i].innerHTML;
+        document.getElementById('display').value += displayI;
+    })
+}
